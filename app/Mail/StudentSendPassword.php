198871a954bcabe4766846f31c08e025a9847de6
DESKTOP-SBDO1HP\Rowan pc <officialvocalstorage@outlook.com>
2018-06-03 20:18:02 +0200

    Send password

---
+++ app/Mail/StudentSendPassword.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Student;
+
+class StudentSendPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $student, $password;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Student $student, $password)
+    {
+        $this->student = $student;
+        $this->password = $password;
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('student.mail.studentSendPassword');
+    }
+}
