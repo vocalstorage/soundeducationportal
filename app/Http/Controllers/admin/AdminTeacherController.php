e2001986bc27fc4d7a9c344e4f3ce30c19290ace
DESKTOP-SBDO1HP\Rowan pc <officialvocalstorage@outlook.com>
2018-06-12 23:44:21 +0200

    Fix

--- app/Http/Controllers/admin/AdminTeacherController.php
+++ app/Http/Controllers/admin/AdminTeacherController.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\TeacherSendPassword;
 use Illuminate\Support\Str;
 use Webpatser\Uuid;
-use App\teacher;
-use App\studio;
+use App\Teacher;
+use App\Studio;
 
 class AdminTeacherController extends Controller
 {
